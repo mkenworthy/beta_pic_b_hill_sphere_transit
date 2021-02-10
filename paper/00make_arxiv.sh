0641427c1b4c5be8d705b321740ade2e3a709f21
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2021-02-10 15:42:34 +0100

    added paper

---
+++ paper/00make_arxiv.sh
@@ -0,0 +1,19 @@
+tar zcvf arxiv_20210210.tgz \
+    ms_2021-02-08_accepted.tex \
+    ms_2021-02-08_accepted.bbl \
+    authorlist.tex \
+    betapic_table.tex \
+    figs/all_binned_photometry.pdf \
+    figs/disk_sketch.jpg \
+    figs/diskfit_ASTEP_030.pdf \
+    figs/diskfit_ASTEP_060.pdf \
+    figs/diskfit_BRING_030.pdf \
+    figs/diskfit_BRING_060.pdf \
+    figs/diskfit_BRITE_030.pdf \
+    figs/diskfit_BRITE_060.pdf \
+    figs/diskfit_taumass_030.pdf \
+    figs/diskfit_taumass_060.pdf \
+    figs/fit_to_1981_model.pdf \
+    figs/hst_visitall-gpfit-maternperiodic.pdf \
+    figs/m1981model.pdf \
+    figs/simdisk_b.pdf
